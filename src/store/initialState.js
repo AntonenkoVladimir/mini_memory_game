80e56d5e96676982720b1827f78a06c116f8c327
AntonenkoVladimir <96226405+AntonenkoVladimir@users.noreply.github.com>
2022-02-28 16:33:08 +0300

    Delete src directory

--- src/store/initialState.js
+++
@@ -1,7 +0,0 @@
-const usersList = JSON.parse(localStorage.getItem("usersList"));
-const currentScore = JSON.parse(localStorage.getItem("currentScore"));
-export const initialState = {
-  usersList: !!usersList ? usersList : [],
-  currentUser: JSON.parse(localStorage.getItem("currentUser")),
-  currentScore: !!currentScore ? currentScore : 0
-};
--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore} from "redux";
-import {reducer} from "./reducers/reducer";
-import {initialState} from "./initialState";
-
-const store = createStore(reducer, initialState);
-
-export default store;
